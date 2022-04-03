147632ed3462846863643dd6a1d904feb1132c0f
Michael Gallagher <navysmeagol0175@gmail.com>
2022-04-02 23:41:07 -0400

    Added Inventory list and loading to PlayerInventory

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
+
+    private List<CharacterInfo> inventory_list;
     // Start is called before the first frame update
+
+
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    void Load()
+    {
+        this.inventory_list.Clear();
+        foreach (CharacterInfo item in Store.Instance.character_list)
+        {
+            if (item.GetObtained())
+            {
+                this.inventory_list.Add(item);
+            }
+
+        }
+
+    }
+
 }
