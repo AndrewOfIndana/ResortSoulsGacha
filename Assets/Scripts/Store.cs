8c018a1270bb67b0fc63680885f52545e238250d
Michael Gallagher <navysmeagol0175@gmail.com>
2022-04-02 23:27:21 -0400

    Implemented Singleton pattern for Store class

--- Assets/Scripts/Store.cs
+++ Assets/Scripts/Store.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class Store : MonoBehaviour
 {
-
+    public static Store Instance { get; private set; }
     //Contains a list of every CharacterInfo object
 
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+
+            Destroy(this);
+        }
+
+        else
+        {
+            Instance = this;
+
+        }
+    }
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
