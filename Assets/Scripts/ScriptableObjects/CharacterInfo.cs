8c675a8da17c6b0b88d95312d3145e42d490e1a0
Michael Gallagher <navysmeagol0175@gmail.com>
2022-04-02 22:57:04 -0400

    Finished Character Info.cs

--- Assets/Scripts/ScriptableObjects/CharacterInfo.cs
+++ Assets/Scripts/ScriptableObjects/CharacterInfo.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class CharacterInfo : ScriptableObject
 {
+    [Header("Name")]
+    public string CharacterName;
+
+    [Header("Pallete")]
     public ReactionPallete reactions;
 
+    [Header("Rarity Info")]
+    public int drop_rate; //indicates how many of them we put into a hat.
 
-    
+    public int tier_value; //0 for unremarkable, 1 for uncanny, 2 for unfathomable
+
+   
+       
 }
---
+++ Assets/Scripts/ScriptableObjects/ReactionPallete.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactionPallete : ScriptableObject
+{
+    public Sprite NEUTRAL;
+    public Sprite HAPPY;
+    public Sprite ANNOYED;
+    public Sprite FLUSTERED;
+    public Sprite LEWD;
+}
